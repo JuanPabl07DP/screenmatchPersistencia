21e13f891874ebb29084a32b287f5c3f20dc697e
JuanPabl07DP <juanpadazer15@gmail.com>
2025-01-12 17:45:15 -0500

    Comentado

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -3,18 +3,18 @@ package com.aluracursos.screenmatch.service;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 
-public class ConsultaChatGPT {
-    public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService("API_KEY_CHATGPT");
-
-        CompletionRequest requisicion = CompletionRequest.builder()
-                .model("gpt-3.5-turbo-instruct")
-                .prompt("traduce a español el siguiente texto: " + texto)
-                .maxTokens(1000)
-                .temperature(0.7)
-                .build();
-
-        var respuesta = service.createCompletion(requisicion);
-        return respuesta.getChoices().get(0).getText();
-    }
-}
+//public class ConsultaChatGPT {
+//    public static String obtenerTraduccion(String texto) {
+//        OpenAiService service = new OpenAiService("API_KEY_CHATGPT");
+//
+//        CompletionRequest requisicion = CompletionRequest.builder()
+//                .model("gpt-3.5-turbo-instruct")
+//                .prompt("traduce a español el siguiente texto: " + texto)
+//                .maxTokens(1000)
+//                .temperature(0.7)
+//                .build();
+//
+//        var respuesta = service.createCompletion(requisicion);
+//        return respuesta.getChoices().get(0).getText();
+//    }
+//}
